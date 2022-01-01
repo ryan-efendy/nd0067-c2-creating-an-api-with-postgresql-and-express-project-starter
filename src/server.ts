72dadb29f457df110e381700f4ce015a9c84a092
ryan efendy <ryanefendy95@gmail.com>
2022-01-01 14:57:53 -0800

    convert function to arrow function

--- src/server.ts
+++ src/server.ts
@@ -6,10 +6,10 @@ const address: string = "0.0.0.0:3000"
 
 app.use(bodyParser.json())
 
-app.get('/', function (req: Request, res: Response) {
+app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!')
 })
 
-app.listen(3000, function () {
+app.listen(3000, () => {
     console.log(`starting app on: ${address}`)
 })
