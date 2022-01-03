607dc1ac21740a22bd51fe0bf787f8ea7c5467d3
ryan efendy <ryanefendy95@gmail.com>
2022-01-03 08:20:15 -0800

    add more intg tests

--- src/models/books.ts
+++ src/models/books.ts
@@ -3,7 +3,7 @@ import client from '../database';
 export type Book = {
     id: number;
     title: string;
-    totalPages: number;
+    total_pages: number;
     author: string;
     type: string;
     summary: string;
@@ -24,11 +24,11 @@ export class BooksStore {
         }
     }
 
-    async show(id: string): Promise<Book> {
+    async show(id: number): Promise<Book> {
         let conn;
         try {
             conn = await client.connect();
-            const sql = 'SELECT * FROM books WHERE id=($1))}';
+            const sql = 'SELECT * FROM books WHERE id=($1)';
             const result = await conn.query(sql, [id]);
             return result.rows[0];
         } catch (error) {
@@ -38,13 +38,25 @@ export class BooksStore {
         }
     }
 
-    async create({ title, totalPages, author, type, summary }: Book): Promise<Book> {
+    async create({
+        title,
+        total_pages,
+        author,
+        type,
+        summary
+    }: {
+        title: string;
+        total_pages: number;
+        author: string;
+        type: string;
+        summary: string;
+    }): Promise<Book> {
         let conn;
         try {
             conn = await client.connect();
             const sql =
                 'INSERT INTO books (title, total_pages, author, type, summary) VALUES ($1, $2, $3, $4, $5) RETURNING *';
-            const result = await conn.query(sql, [title, totalPages, author, type, summary]);
+            const result = await conn.query(sql, [title, total_pages, author, type, summary]);
             return result.rows[0];
         } catch (error) {
             throw new Error(`Could not add new book ${title}. Error: ${error}`);
@@ -53,7 +65,7 @@ export class BooksStore {
         }
     }
 
-    async delete(id: string): Promise<Book> {
+    async delete(id: number): Promise<Book> {
         let conn;
         try {
             conn = await client.connect();
