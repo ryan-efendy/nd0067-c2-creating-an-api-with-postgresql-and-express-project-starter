bd8f55615fd5445ed7b0183a44e4aa9bd75ed60e
ryan efendy <ryanefendy95@gmail.com>
2022-01-03 08:19:32 -0800

    add a test db

--- src/database.ts
+++ src/database.ts
@@ -1,16 +1,22 @@
-import dotenv from "dotenv";
-import { Pool } from "pg";
+import 'dotenv/config';
+import { Pool } from 'pg';
 
-dotenv.config();
+const { ENV, POSTGRES_HOST, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_TEST_DB } = process.env;
 
-const { POSTGRES_HOST, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD } =
-  process.env;
-
-const client = new Pool({
-  host: POSTGRES_HOST,
-  database: POSTGRES_DB,
-  user: POSTGRES_USER,
-  password: POSTGRES_PASSWORD,
+let client = new Pool({
+    host: POSTGRES_HOST,
+    database: POSTGRES_DB,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD
 });
+
+if (ENV == 'test') {
+    client = new Pool({
+        host: POSTGRES_HOST,
+        database: POSTGRES_TEST_DB,
+        user: POSTGRES_USER,
+        password: POSTGRES_PASSWORD
+    });
+}
 
 export default client;
