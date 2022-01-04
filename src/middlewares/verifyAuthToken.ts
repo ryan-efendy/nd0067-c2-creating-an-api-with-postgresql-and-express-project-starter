0d1b350aa97c29070779da1c02ae8eff2268489f
ryan efendy <ryanefendy95@gmail.com>
2022-01-04 13:09:48 -0800

    reorganize middlewares

---
+++ src/middlewares/verifyAuthToken.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+export const verifyAuthToken = (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const authorizationHeader = req.headers.authorization;
+        if (authorizationHeader) {
+            const token = authorizationHeader.split(' ')[1];
+            jwt.verify(token, process.env.TOKEN_SECRET!);
+            next();
+        }
+        res.status(401);
+        res.json('Access denied, invalid token');
+        return;
+    } catch (error) {
+        res.status(401);
+        res.json('Access denied, invalid token');
+        return;
+    }
+};
