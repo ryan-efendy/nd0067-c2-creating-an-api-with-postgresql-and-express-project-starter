bff386e185229b76956516327293e05baa7e5442
ryan efendy <ryanefendy95@gmail.com>
2022-01-04 14:18:35 -0800

    clean up unnecessary config in database.ts

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,7 @@
 import 'dotenv/config';
 import { Pool } from 'pg';
 
-const { ENV, POSTGRES_HOST, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_TEST_DB, POSTGRES_DEV1_DB } =
-    process.env;
+const { ENV, POSTGRES_HOST, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_TEST_DB } = process.env;
 
 console.log(`ENV: ${ENV}`);
 
@@ -17,15 +16,6 @@ if (ENV == 'test') {
     client = new Pool({
         host: POSTGRES_HOST,
         database: POSTGRES_TEST_DB,
-        user: POSTGRES_USER,
-        password: POSTGRES_PASSWORD
-    });
-}
-
-if (ENV == 'dev1') {
-    client = new Pool({
-        host: POSTGRES_HOST,
-        database: POSTGRES_DEV1_DB,
         user: POSTGRES_USER,
         password: POSTGRES_PASSWORD
     });
