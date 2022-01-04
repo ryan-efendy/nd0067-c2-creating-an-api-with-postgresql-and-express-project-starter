cc828de1cfc24bdda20966c7d9881a0097b8f9b1
ryan efendy <ryanefendy95@gmail.com>
2022-01-04 13:10:47 -0800

    update route index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express';
-import products from './product';
+import products from './api/product';
+import users from './api/user';
+
 const routes = Router();
 
 routes.use('/products', products);
+routes.use('/users', users);
 export default routes;
