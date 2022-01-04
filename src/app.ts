ab93585eb1b94c2269f4293dd7aafc3191ff46a9
ryan efendy <ryanefendy95@gmail.com>
2022-01-04 13:09:30 -0800

    remove unused route

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,6 @@
 import express, { Request, Response } from 'express';
 import pinoHttp from 'pino-http';
 import routes from './routes';
-import articles_routes from './routes/article';
-import users from './routes/user';
 import logger from './util/logger';
 
 const app = express();
@@ -15,9 +13,5 @@ app.get('/', (_req: Request, res: Response) => res.send('👋 Hello World!'));
 app.get('/health', (_req: Request, res: Response) => res.send({ message: '👍' }));
 
 app.use('/api', routes);
-
-articles_routes(app);
-
-app.use('/users', users);
 
 export default app;
