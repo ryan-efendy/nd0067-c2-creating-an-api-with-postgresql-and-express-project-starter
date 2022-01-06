1579b81ca62b2ad8946e7d38f4e9ce6b2a1f4ceb
ryan efendy <ryanefendy95@gmail.com>
2022-01-05 22:49:54 -0800

    add e2e tests

---
+++ tests/integration/app.test.ts
@@ -0,0 +1,18 @@
+import request from 'supertest';
+import app from '../../src/app';
+
+describe('GET /', () => {
+    test('should respond with a 200 status code', async () => {
+        const response = await request(app).get('/');
+        expect(response.statusCode).toBe(200);
+        expect(response.text).toBe('👋 Hello World!');
+    });
+});
+
+describe('GET /health', () => {
+    test('should respond with a 200 status code', async () => {
+        const response = await request(app).get('/health');
+        expect(response.statusCode).toBe(200);
+        expect(response.body.message).toBe('👍');
+    });
+});
