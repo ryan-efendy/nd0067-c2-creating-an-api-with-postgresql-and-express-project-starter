d3bf339283f3ad99e72f7b1e521c4aee5c1ba446
ryan efendy <ryanefendy95@gmail.com>
2022-01-05 22:53:09 -0800

    add jwt authentication to all order routes

--- src/routes/api/order.ts
+++ src/routes/api/order.ts
@@ -1,16 +1,17 @@
 import { Router } from 'express';
 import { getOrders, getOrder, createOrder, deleteOrder, addProduct } from '../../controllers/order';
+import { verifyAuthToken } from '../../middlewares/verifyAuthToken';
 
 const orders = Router();
 
-orders.get('/', getOrders);
+orders.get('/', verifyAuthToken, getOrders);
 
-orders.get('/:id', getOrder);
+orders.get('/:id', verifyAuthToken, getOrder);
 
-orders.post('/', createOrder);
+orders.post('/', verifyAuthToken, createOrder);
 
-orders.delete('/:id', deleteOrder);
+orders.delete('/:id', verifyAuthToken, deleteOrder);
 
-orders.post('/:id/products', addProduct);
+orders.post('/:id/products', verifyAuthToken, addProduct);
 
 export default orders;
