db9bd9c2c3cd1b2c3e08221ccb982046331b6025
ryan efendy <ryanefendy95@gmail.com>
2022-01-06 18:42:23 -0800

    add tests to add product to order and fetch products for an order

--- src/models/tests/order.test.ts
+++ src/models/tests/order.test.ts
@@ -1,6 +1,6 @@
-import { Order, OrderStore } from './order';
-import { Product, ProductStore } from './product';
-import { User, UserStore } from './user';
+import { Order, OrderStore } from '../order';
+import { ProductStore } from '../product';
+import { UserStore } from '../user';
 
 const orderStore = new OrderStore();
 const productStore = new ProductStore();
@@ -28,76 +28,44 @@ describe('OrderStore', () => {
     });
 
     it('create method should add an order - active', async () => {
-        let product = await productStore.show(1);
-        if (!product) {
-            const newProduct: Product = {
-                name: 'book3',
-                price: 1.23,
-                category: 'Books'
-            };
-            product = await productStore.create(newProduct);
-        }
-
-        let user = await userStore.show(1);
-        if (!user) {
-            const newUser: User = {
-                firstname: 'John',
-                lastname: 'Doe',
-                password: 'password'
-            };
-            user = await userStore.create(newUser);
-        }
+        const user = await userStore.create({
+            firstname: 'John',
+            lastname: 'Doe',
+            password: 'password'
+        });
 
         const order: Order = {
-            product_id: product.id!,
             user_id: user.id!,
             quantity: 1,
             status: 'active'
         };
 
         const result = await orderStore.create(order);
-        expect(result.product_id).toEqual(order.product_id);
         expect(result.user_id).toEqual(order.user_id);
         expect(result.quantity).toEqual(order.quantity);
         expect(result.status).toEqual(order.status);
     });
 
     it('create method should add an order - complete', async () => {
-        let product = await productStore.show(2);
-        if (!product) {
-            const newProduct: Product = {
-                name: 'book3',
-                price: 1.23,
-                category: 'Books'
-            };
-            product = await productStore.create(newProduct);
-        }
-
-        let user = await userStore.show(2);
-        if (!user) {
-            const newUser: User = {
-                firstname: 'John',
-                lastname: 'Doe',
-                password: 'password'
-            };
-            user = await userStore.create(newUser);
-        }
+        const user = await userStore.create({
+            firstname: 'Jane',
+            lastname: 'Doe',
+            password: 'password'
+        });
 
         const order: Order = {
-            product_id: product.id!,
             user_id: user.id!,
             quantity: 1,
             status: 'complete'
         };
 
         const result = await orderStore.create(order);
-        expect(result.product_id).toEqual(order.product_id);
         expect(result.user_id).toEqual(order.user_id);
         expect(result.quantity).toEqual(order.quantity);
         expect(result.status).toEqual(order.status);
     });
 
-    it('index method should return a list of products', async () => {
+    it('index method should return a list of orders', async () => {
         const result = await orderStore.index();
         expect(result.length).toBeGreaterThan(0);
     });
@@ -114,5 +82,44 @@ describe('OrderStore', () => {
                 expect(user_id).toEqual(orders[0].user_id);
             });
         }
+    });
+
+    it('addProduct method should add a product to an order and return the updated order', async () => {
+        const product = await productStore.create({
+            name: 'book3',
+            price: 1.23,
+            category: 'Books'
+        });
+
+        const user = await userStore.create({
+            firstname: 'Rhea',
+            lastname: 'McLaughlin',
+            password: 'password'
+        });
+
+        const order = await orderStore.create({
+            user_id: user.id!,
+            quantity: 1,
+            status: 'active'
+        });
+
+        await orderStore.addProduct(1, order.id!, product.id!);
+
+        let products = await orderStore.getProducts(order.id!);
+        expect(products.length).toBeGreaterThan(0);
+
+        const currentNumberOfProducts = products.length;
+
+        const anotherProduct = await productStore.create({
+            name: 'book4',
+            price: 2.46,
+            category: 'Books'
+        });
+
+        await orderStore.addProduct(2, order.id!, anotherProduct.id!);
+
+        products = await orderStore.getProducts(order.id!);
+
+        expect(products.length).toStrictEqual(currentNumberOfProducts + 1);
     });
 });
